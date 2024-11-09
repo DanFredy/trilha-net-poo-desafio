bbb0a21f5eddbc2c384d79b02fdd25a8ce838021
Daniel Alfredo <danfredy@icloud.com>
2024-11-09 04:18:50 -0300

    Desafio POO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    public class Iphone : Smartphone
+    {   
+       public Iphone(string numero) : base(numero)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+
+        }
     }
 }
